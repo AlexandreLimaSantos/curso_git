2afe6f243cb34cc7327a737e38be1a50d17f158f
alexandre <alexabdrelymasasntos@gmail.com>
2019-04-13 10:00:57 -0300

    modificacao no printf

---
+++ v1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int *v1,*v2,n,i;
+	printf("\nDigite o tamanho do vetor\n");
+	scanf("%d",&n);
+	v1 = (int)malloc(sizeof(int)*n)
+	return 0;
+}
